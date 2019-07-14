4e982d8886eaf8dcb2255c2216dad8757e88d5fb
Rogers <james.rogers@jhuapl.edu>
2019-07-14 13:01:41 -0400

    Added socket comms to the various subsystems

--- board.py
+++ board.py
@@ -1,8 +1,30 @@
 #!/bin/env python3
 
+import socket
+
 class Board:
     def __init__(self):
+        self.receiver = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.receiver.bind(("127.0.0.1", 10001))
+        self.receiver.listen(2)
+        # Keep trying to create the sender until the correct receiver has been created
+        while True:
+            try:
+                self.sender = socket.connect("127.0.0.1", 10011)
+                break
+            except:
+                continue
+        getCommand()
+
+    def flipQuestion(self, category):
+        # Flip over the correct category card
         pass
-    def doSpin(self):
-        # yield a number in range?
-        pass
+
+    def getCommand():
+        while True:
+            command = self.receiver.recv()
+                if (command == 1):
+                    flipQuestion()
+                    self.sender.send("ACK")
+                else:
+                    continue
