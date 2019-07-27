ce243fc576b66a67aec5774edd44528bfad4ca9b
awhite <awhite@localhost>
2019-07-27 13:48:22 -0400

    Deploy python logging to trivia classes

--- trivia.py
+++ trivia.py
@@ -3,11 +3,15 @@
 
 import json
 import random
+import logging
+import logs
 
 
 class Trivia:
 
-    def __init__(self, question_file='qa.json', min_questions=0):
+    def __init__(self, loglevel=logging.INFO, question_file='qa.json', min_questions=0):
+        self.logger = logs.build_logger(__name__, loglevel)
+        self.loglevel = loglevel
         # read contents of question list
         self.debug = False
         self.raw = self.readQuestionFile(question_file)
@@ -16,6 +20,7 @@ class Trivia:
         self.db = self.assemble_db()
 
         pass
+
     def __len__(self):
         return len(self.raw)
 
@@ -30,15 +35,15 @@ class Trivia:
         """Consolidate JSON-oriented Quester/Answer File into a Dictionary"""
         # rather than read the file multiple times, we convert to a dictionary first
         db = {}
-        if self.debug: print(type(self.raw))
+        self.logger.debug("type(self.raw)=" + str(type(self.raw)))
         for item in self.raw:
-            if self.debug: print(type(item))
-            if self.debug: print(item)
+            self.logger.debug("type(item)=" + str(type(item)))
+            self.logger.debug("item=" + str(item))
             if item['category'] not in db.keys():
                 db[item['category']] = []
             db[item['category']].append(item)
         if removeDisqualifyingCategories:
-            for cat in db.keys():
+            for cat in list(db):
                 if len(db[cat]) < self.min_questions:
                     db.pop(cat)
         return db
@@ -46,25 +51,27 @@ class Trivia:
     def selectRandomCategories(self, n, exclude=[], n_questions_per_category=-1):
         """Return n Topic Categories"""
         categories = [x for x in self.db.keys() if x not in exclude]
-        if self.debug: print("selectRandomCategories(): categories = ", categories)
+        self.logger.debug("categories = " + str(categories))
         if len(categories) < n:
             raise Exception("Cannot gather sufficient categories to cover request of", n)
 
         selected_categories = []
         while len(selected_categories) < n:
             r = random.randrange(0, len(categories))
-            if self.debug: print("selectRandomCategories(): r= ", r, "n=", n, "len(selected_categories)=", len(selected_categories))
+            self.logger.debug("r=" + str(r) + " n=" + str(n) + " len(selected_categories)="
+                              + str(len(selected_categories)))
             if categories[r] not in selected_categories:
                 selected_categories.append(categories[r])
-        if self.debug: print("selectRandomCategories(): selected_categories=", selected_categories)
+        self.logger.debug("selected_categories=" + str(selected_categories))
         if n_questions_per_category > 0:
             return_list = []
             for category in selected_categories:
                 num_questions = len(self.db[category])
-                if self.debug: print("selectRandomCategories(): num_questions=", num_questions, "n_questions_per_category=", n_questions_per_category)
+                self.logger.debug("num_questions=" + str(num_questions) +
+                                  " n_questions_per_category=" + str(n_questions_per_category))
                 if num_questions == n_questions_per_category:
                     return_list.append({"category": category, "topics": self.db[category]})
-                    if self.debug: print("selectRandomCategories(): return_list=", return_list)
+                    self.logger.debug("return_list=" + str(return_list))
                 else:
                     hitlist = []
                     selection = []
